d298fade40fac8e394913f8814e9d1be5304bae1
SudipGhara19 <sudipghara135@gmail.com>
2023-05-04 16:55:32 +0530

    added post-details to the postdetails.js using useEffect

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,5 +1,27 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { firestore } from "../firebase";
+
+
 function PostDetail(){
-    return( <div className="">PostDetail</div>);
+  const [post, setPost] = useState({});
+  const { postId } = useParams();
+
+  useEffect(() => {
+    firestore.collection('posts').doc(postId).get().then((snapshot) => {
+      setPost(snapshot.data());
+    })
+  }, []);
+
+
+    return( 
+    <div className="post-detail">
+      <h1>{post.title}</h1>
+      <h2>{post.subTitle}</h2>
+      <h3>{post.content}</h3>
+      <p>{post.time}</p>
+    </div>
+    );
   }
   
   export default PostDetail;
